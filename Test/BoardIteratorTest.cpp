7f8abf5800321ca466b1a7bde888e5c0c12dcb65
PLUkraine <plukraine001@gmail.com>
2018-09-04 11:20:27 +0300

    Split project into 3 projects

--- Test/BoardIteratorTest.cpp
+++ Test/BoardIteratorTest.cpp
@@ -8,6 +8,16 @@ private slots:
         HorizontalIterator it(4, 3, 3);
         QVERIFY(it.hasNext());
         QVERIFY(it.getNext() == 5);
+
+        QVERIFY(!it.hasNext());
+
+        QVERIFY(it.hasPrev());
+        QVERIFY(it.getPrev() == 4);
+
+        QVERIFY(it.hasPrev());
+        QVERIFY(it.getPrev() == 3);
+
+        QVERIFY(!it.hasPrev());
     }
 };
 
